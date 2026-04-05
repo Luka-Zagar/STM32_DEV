0efbc99aa70dc1fb5d7c00c1230556358df6eb38
Luka Zagar <lukazagar64@gmail.com>
2026-04-05 12:00:36 +0200

    Initial commit for EkoSonda

---
+++ Src/main.c
@@ -0,0 +1,32 @@
+/**
+ ******************************************************************************
+ * @file        main.c
+ * @project     EURUS — Environmental Urban Roaming Unified Sensoring
+ * @author      Luka Zagar (student at University of Ljubljana, Faculty of Electrical Engineering)
+ * 
+ * @date        5th April 2026
+ * @brief       Main program entry point
+ *
+ * @board       NUCLEO-G474RE
+ * @mcu         STM32G474RET6 — ARM Cortex-M4 @ 170MHz, 512KB Flash, 128KB RAM
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+/* ── Register Base Addresses ──────────────────────────────────────────────── */
+
+
+/* ── Pin Definitions ──────────────────────────────────────────────────────── */
+
+
+/* ── Private Function Declarations ────────────────────────────────────────── */
+
+
+/* ── Main ─────────────────────────────────────────────────────────────────── */
+int main(void) {
+
+    /* Safety trap incase main ever returns */
+    while(1) {}
+}
